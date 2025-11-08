f927e210a7ae500ba491eda775f719454fd0d15a
Filip Paciorek <filip_paciorek@wp.pl>
2025-11-08 23:00:36 +0100

    Added models with less telling features to tell a better story

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -6,18 +6,27 @@ data_raw = data_raw[data_raw['stalk-root']!='?'].copy()
 #data_raw.drop(columns=['Unnamed: 0','Unnamed: 0.1','Unnamed: 0.2'],inplace=True)
 #data_raw.to_csv('../data/raw/data_raw',index=False)
 results = get_ordinal_results()
-def base_model_features(data_raw):
+def no_obvious_features(state):
+    if state == 'mild':
+        return ['odor','gill-color','spore-print-color']
+    return []
+def base_model_features(data_raw,state=None):
+    data_raw = data_raw.drop(no_obvious_features(state),axis=1)
     data_clean = data_raw
     for column in data_raw:
         if column == 'class':
-            data_raw[data_raw['class']=='EDIBLE'] = 1
-            data_raw[data_raw['class']=='POISONOUS']=0
+            data_clean.loc[data_raw['class']=='EDIBLE','class'] = 1
+            data_clean.loc[data_raw['class']=='POISONOUS','class']=0
             continue
         data_clean = pd.get_dummies(data_clean,columns=[column])
     return data_clean
-def ordinal_max_features(data_raw,ord_columns):
+def ordinal_max_features(data_raw,ord_columns,state=None):
+    rejected_columns = no_obvious_features(state)
+    data_raw = data_raw.drop(rejected_columns,axis=1)
     data_clean = data_raw
     for column in ord_columns:
+        if column in rejected_columns:
+            continue
         safety_score = len(ord_columns[column])
         if column == 'class':
             data_clean.loc[data_clean[column] == 'EDIBLE',column] = 1
@@ -36,11 +45,11 @@ def ordinal_max_features(data_raw,ord_columns):
     return data_clean
 
 data_clean = ordinal_max_features(data_raw,results)
-print(data_clean)
 data_clean.to_csv('../data/processed/max_ordinal_data',index=False)
-
-        
-    
+data_clean = ordinal_max_features(data_raw,results,'mild')
+data_clean.to_csv('../data/processed/no_obvious_data_ordinal',index=False)
+data_clean = base_model_features(data_raw,'mild')
+data_clean.to_csv('../data/processed/no_obvious_data',index=False)    
 data_clean = base_model_features(data_raw)
 data_clean.to_csv('../data/processed/base_model_data',index=False)
 #print(data_raw['stalk-root'].nunique())
--- src/regression_model.py
+++ src/regression_model.py
@@ -4,6 +4,8 @@ import sys
 #import feature models
 base_data = pd.read_csv('../data/processed/base_model_data')
 max_ord_data = pd.read_csv('../data/processed/max_ordinal_data')
+no_obvious_data_base = pd.read_csv('../data/processed/no_obvious_data')
+no_obvious_date_ordinal = pd.read_csv('../data/processed/no_obvious_data_ordinal')
 def train_test_split(X,y,size,state):
     N = len(X)
     n_test = int(N*size)
@@ -14,14 +16,13 @@ def train_test_split(X,y,size,state):
     train_indx = indx[n_test:]
     X_train = X.iloc[train_indx].copy()
     X_test = X.iloc[test_indx].copy()
-    #X_train = (X_train - X_train.mean()) / X_train.std()
-    #X_test = (X_test - X_train.mean()) / X_train.std()
     y_train = y.iloc[train_indx].copy()
     y_test = y.iloc[test_indx].copy()
     return X_train,X_test,y_train,y_test
 def sigmoid(a):
+    eps = 1e-12
     return 1/(1+np.exp(-a))
-def predict(w,b,y):
+def predict(y_pred):
     y_pred = pd.Series(y_pred)
     y_pred = y_pred.mask(y_pred >= 0.5,1)
     y_pred = y_pred.mask(y_pred < 0.5,0)
@@ -50,13 +51,6 @@ def gradient_descent(X,y,batch,iter):
             z = np.dot(xbatch,w)+b
             z = z.astype(float)
             a = sigmoid(z)
-            #print(f"--- Batch {i} Debug ---")
-            #print(f"Type of 'a': {type(a)}, Dtype of 'a': {a.dtype}")
-            #print(f"Type of 'ybatch': {type(ybatch)}, Dtype of 'ybatch': {ybatch.dtype}")
-            #print(f"Mean of 'a': {a.mean():.4f}")
-            #print(f"Mean of 'ybatch': {ybatch.mean():.4f}")
-            #break
-            #calculate the error
             error = a - ybatch
             #optimize w and b
             d_w = (np.transpose(xbatch) @ error) / batch
@@ -65,7 +59,6 @@ def gradient_descent(X,y,batch,iter):
             b = b - (lr*d_b)
             y_pred = np.append(y_pred,a)
             #print(w,b)
-            #print(f"Batch {i}: y_batch_mean={ybatch.mean():.2f}, d_b={d_b:.4f}, new_b={b:.4f}")
         if data_left:
             xbatch = X[len(y)-data_left:]
             ybatch = y[len(y)-data_left:]
@@ -78,22 +71,52 @@ def gradient_descent(X,y,batch,iter):
             w = w - (lr*d_w)
             b = b - (lr*d_b)
             y_pred = np.append(y_pred,a)
-        #y_pred = predict(y_pred)
-        #print(y_pred)
         print(cross_enthropy_loss(y,y_pred))
     return w,b
+def predict_final(X,w,b):
+    y_pred = []
+    z = np.dot(X,w)+b
+    z = z.astype(float)
+    a = sigmoid(z)
+    y_pred = np.append(y_pred,a)
+    return y_pred
+def compare(y,y_pred):
+    correct = 0
+    incorrect = 0
+    y_pred = predict(y_pred)
+    y = np.array(y)
+    for val in range(len(y_pred)):
+        if y_pred[val] == y[val]:
+            correct += 1
+        else:
+            incorrect += 1
+    sum = correct + incorrect
+    print(f'Correct guesses: {correct}/{sum}')
+    print(f'Incorrect guesses: {incorrect}/{sum}')
 X = base_data.drop(columns=['class'])
 y = base_data['class'] 
-X_train,X_test,y_train,y_test = train_test_split(X,y,0.2,100)
+X_train,X_test,y_train,y_test = train_test_split(X,y,0.2,103)
 w,b = gradient_descent(X_train,y_train,256,10)
-#predicted = predict(y_pred)
-print(w,b)
+y_pred = predict_final(X_test,w,b)
+compare(y_test,y_pred)
+print('---')
+X = no_obvious_data_base.drop(columns=['class'])
+y = no_obvious_data_base['class'] 
+X_train,X_test,y_train,y_test = train_test_split(X,y,0.2,103)
+w,b = gradient_descent(X_train,y_train,256,10)
+y_pred = predict_final(X_test,w,b)
+compare(y_test,y_pred)
 print('---')
 X = max_ord_data.drop(columns=['class'])
 y = max_ord_data['class']
-X_train,X_test,y_train,y_test = train_test_split(X,y,0.2,100)
+X_train,X_test,y_train,y_test = train_test_split(X,y,0.2,103)
 w,b = gradient_descent(X_train,y_train,256,10)
-#predict(y_pred)
-print(w,b)
-print('Feature distribution')
-print(y_train.unique())
+y_pred = predict_final(X_test,w,b)
+compare(y_test,y_pred)
+print(set(X_train.index).intersection(X_test.index))
+X = no_obvious_date_ordinal.drop(columns=['class'])
+y = no_obvious_date_ordinal['class']
+X_train,X_test,y_train,y_test = train_test_split(X,y,0.2,103)
+w,b = gradient_descent(X_train,y_train,256,10)
+y_pred = predict_final(X_test,w,b)
+compare(y_test,y_pred)
